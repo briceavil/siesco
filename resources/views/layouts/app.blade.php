d782fbed3d5a8521ca19e4898e523d6542ae1304
Daniel Briceño <danielbr59@hotmail.com>
2024-07-03 16:06:21 -0400

    reacomodo de notify js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,6 @@
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
         @include('layouts.navigation')
         <x-notify::notify />
-        @notifyJs
         <!-- Page Heading -->
         @isset($header)
         <header class="bg-white dark:bg-gray-800 shadow">
@@ -86,6 +85,7 @@
 
         });
     </script>
+    @notifyJs
 </body>
 
 </html>
